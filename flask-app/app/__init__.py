e0fae1a3fb95f5638be090451041f574eb592c55
thiago002125 <102823398+thiago002125@users.noreply.github.com>
2022-10-26 14:20:12 -0300

    Add files via upload

---
+++ flask-app/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_cors import CORS
+
+app=Flask(__name__)
+CORS(app)
+app.config.from_object("config")
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command("db", MigrateCommand)
+
+from app.model.autor import Autor
+from app.model.post import Post
+
+from app.controler import autor_controller
+from app.controler import post_controller
